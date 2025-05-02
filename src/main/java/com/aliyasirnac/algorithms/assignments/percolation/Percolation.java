a81cb71ac2357a18e0012b1b9defe94b8ea6a628
aliyasirnac <aliyasirnac@gmail.com>
2025-05-02 04:22:09 +0300

    percolation assignments is done

--- src/main/java/com/aliyasirnac/algorithms/assignments/percolation/Percolation.java
+++ src/main/java/com/aliyasirnac/algorithms/assignments/percolation/Percolation.java
@@ -1,4 +1,73 @@
 package com.aliyasirnac.algorithms.assignments.percolation;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
+    private final boolean[][] grid;
+    private final int n;
+    private final WeightedQuickUnionUF uf;
+    private int openSites;
+    private final int virtualTop;
+    private final int virtualBottom;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("n must be greater than 0");
+        }
+        grid = new boolean[n][n];
+        this.n = n;
+        this.uf = new WeightedQuickUnionUF(n * n + 2);
+        virtualTop = n * n;
+        virtualBottom = n * n + 1;
+    }
+
+    private int check(int row, int col) {
+        return (row - 1) * n + (col - 1);
+    }
+
+    private void validate(int row, int col) {
+        if (row < 1 || row > n || col < 1 || col > n) {
+            throw new IllegalArgumentException("Invalid row or col");
+        }
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        validate(row, col);
+        if (isOpen(row, col)) return;
+        grid[row - 1][col - 1] = true;
+        openSites++;
+
+        int current = check(row, col);
+        if (row == 1) uf.union(current, virtualTop);
+        if (row == n) uf.union(current, virtualBottom);
+
+        if (row > 1 && isOpen(row - 1, col)) uf.union(current, check(row - 1, col));
+        if (row < n && isOpen(row + 1, col)) uf.union(current, check(row + 1, col));
+        if (col > 1 && isOpen(row, col - 1)) uf.union(current, check(row, col - 1));
+        if (col < n && isOpen(row, col + 1)) uf.union(current, check(row, col + 1));
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        validate(row, col);
+        return grid[row - 1][col - 1];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        validate(row, col);
+        return uf.find(check(row, col)) == uf.find(virtualTop);
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return uf.find(virtualTop) == uf.find(virtualBottom);
+    }
 }
--- src/main/java/com/aliyasirnac/algorithms/assignments/percolation/PercolationStats.java
+++ src/main/java/com/aliyasirnac/algorithms/assignments/percolation/PercolationStats.java
@@ -1,16 +1,27 @@
 package com.aliyasirnac.algorithms.assignments.percolation;
 
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private final int[] percolationStats;
+    private final double[] percolationStats;
+    private final int trials;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
-        percolationStats = new int[n * n];
+        if (n <= 0 || trials <= 0) throw new IllegalArgumentException("n or trials must be positive");
+
+        this.trials = trials;
+        percolationStats = new double[trials];
         for (int i = 0; i < trials; i++) {
-            percolationStats[i] = 1;
+            Percolation percolation = new Percolation(n);
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniformInt(1, n + 1);
+                int col = StdRandom.uniformInt(1, 1 + n);
+                percolation.open(row, col);
+            }
+            percolationStats[i] = percolation.numberOfOpenSites();
         }
     }
 
@@ -26,17 +37,23 @@ public class PercolationStats {
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return mean() - (stddev() / Math.sqrt(percolationStats.length));
+        return mean() - (1.96 * stddev() / Math.sqrt(trials));
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return mean() + (stddev() / Math.sqrt(percolationStats.length));
+        return mean() + (1.96 * stddev() / Math.sqrt(trials));
     }
 
     // test client (see below)
     public static void main(String[] args) {
-        PercolationStats stats = new PercolationStats(5, 5);
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        if (trials <= 0) {
+            throw new IllegalArgumentException("trials must be positive");
+        }
+
+        PercolationStats stats = new PercolationStats(n, trials);
         StdOut.println("mean = " + stats.mean());
         StdOut.println("stddev = " + stats.stddev());
         StdOut.println("95% confidence interval = [" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
