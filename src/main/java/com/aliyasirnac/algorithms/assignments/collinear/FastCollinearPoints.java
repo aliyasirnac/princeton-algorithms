c5f777c429eb089afe9b47f01cd7032f391a9c72
aliyasirnac <aliyasirnac@gmail.com>
2025-11-26 07:37:11 +0300

    Collinear points assignment started

---
+++ src/main/java/com/aliyasirnac/algorithms/assignments/collinear/FastCollinearPoints.java
@@ -0,0 +1,16 @@
+package com.aliyasirnac.algorithms.assignments.collinear;
+
+public class FastCollinearPoints {
+    public FastCollinearPoints(Point[] points) {
+
+    } // finds all line segments containing 4 or more points
+
+    public int numberOfSegments() {
+        return 0;
+    }    // the number of line segments
+
+    public LineSegment[] segments() {
+        return null;
+    }      // the line segments
+
+}
