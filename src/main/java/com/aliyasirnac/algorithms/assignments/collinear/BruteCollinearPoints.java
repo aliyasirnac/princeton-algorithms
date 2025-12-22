4dd1287c2e1eca2dfacbb6a014b23e91a2547054
Ali Yasir Naç <aliyasirnac@gmail.com>
2025-12-23 00:55:45 +0300

    LineSegment arrayi ve arraylisti oluşturuldu

--- src/main/java/com/aliyasirnac/algorithms/assignments/collinear/BruteCollinearPoints.java
+++ src/main/java/com/aliyasirnac/algorithms/assignments/collinear/BruteCollinearPoints.java
@@ -1,11 +1,14 @@
 package com.aliyasirnac.algorithms.assignments.collinear;
 
+import java.util.ArrayList;
+
 public class BruteCollinearPoints {
+    private final LineSegment[] lineSegments;
     public BruteCollinearPoints(Point[] points) {
         if (points == null) {
             throw new IllegalArgumentException();
         }
-
+        ArrayList<LineSegment> segments = new ArrayList<>();
         for (Point p : points) {
             for (Point q : points) {
                 for (Point r : points) {
@@ -15,14 +18,15 @@ public class BruteCollinearPoints {
                 }
             }
         }
+        lineSegments = segments.toArray(new LineSegment[0]);
     }   // finds all line segments containing 4 points
 
     public int numberOfSegments() {
-        return 0;
-    }     // the number of line segments
+        return lineSegments.length;
+    }
 
     public LineSegment[] segments() {
-        return null;
+        return lineSegments.clone();
     }
 
 }
