d149489b5eea27f2081e7b97b043c5a2a56c85e7
Ali Yasir Naç <aliyasirnac@gmail.com>
2026-01-15 06:21:24 +0300

    Board.java dosyasında toString, dimension ve constructor yazıldı

--- src/main/java/com/aliyasirnac/algorithms/assignments/puzzle8/Board.java
+++ src/main/java/com/aliyasirnac/algorithms/assignments/puzzle8/Board.java
@@ -1,21 +1,48 @@
 package com.aliyasirnac.algorithms.assignments.puzzle8;
 
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.Iterator;
 
+import static java.lang.System.*;
+
 public class Board {
+    private final int[][] board;
+    private final int n;
+
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
+        if (tiles == null) {
+            throw new IllegalArgumentException();
+        }
+        n = tiles.length;
+        board = new int[n][n];
+        for (int col = 0; col < tiles.length; col++) {
+            arraycopy(tiles[col], 0, board[col], 0, tiles.length);
+        }
     }
 
     // string representation of this board
     public String toString() {
-        return "";
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int j = 0;
+            while (j < n) {
+                int currentItem = board[i][j];
+                s.append(currentItem).append(" ");
+                if (i < j && currentItem % n == 0) {
+                    s.append("\n");
+                }
+                j++;
+            }
+        }
+        return dimension() + "\n" + s;
     }
 
     // board dimension n
     public int dimension() {
-        return 0;
+        return n;
     }
 
     // number of tiles out of place
@@ -50,11 +77,18 @@ public class Board {
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
-        return new Board(null);
+        return new Board(board);
     }
 
     // unit testing (not graded)
     public static void main(String[] args) {
-
+        int[][] ints = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 0} // 0 represents the blank
+        };
+        Board board = new Board(ints);
+        StdOut.println("Dimension: " + board.dimension());
+        StdOut.println("Board:\n" + board);
     }
 }
